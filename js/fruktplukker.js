d7a78c6bd0479f1fd0792166af4e2f74f10b8846
Henry <Hensjfl@gmail.com>
2025-10-03 13:55:00 +0200

    Lagt til poeng

--- js/fruktplukker.js
+++ js/fruktplukker.js
@@ -9,7 +9,7 @@ function nyFrukt()
 {
     const frukt = document.createElement("div");
     frukt.innerHTML = "🍎"; // Du kan endre dette til forskjellige frukt emojis
-    frukt.style.fontSize = "2em";
+    frukt.style.fontSize = "3em";
     frukt.style.position = "absolute";
     frukt.style.left = Math.random() * window.innerWidth + 'px'; // Plasser frukten på en tilfeldig x-posisjon
     frukt.style.top = Math.random() * window.innerHeight + 'px'; // Plasser frukten på en tilfeldig y-posisjon
@@ -19,10 +19,16 @@ function nyFrukt()
     frukt.addEventListener("click", fjernFrukt)
 }
 
+    let eplePoeng = document.querySelector("#Poeng")
+    eplePoeng.innerHTML = 0
+    let Poeng = 0
 /* Legg merke til bokstaven e inne i parentesen på linja under. 
 Dette betyr at vi sender informasjon om hendelsen (event) som trigget funksjonen inn i funksjonen. e kalles hendelses-objektet */
 function fjernFrukt(e)
 {
     document.body.removeChild(e.target); 
+    Poeng+= 1
+    eplePoeng.innerHTML = Poeng
+    
     //e.target er det elementet som trigget hendelsen, det vil si elementet vi klikket på for å aktivere funksjonen.
 }
